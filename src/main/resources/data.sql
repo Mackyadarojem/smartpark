d1b286496e2ec4bb8cf6ea9b12c9034defb04b99
Macky <mackymejorada@gmail.com>
2026-08-16 17:16:28 +0800

    Add Dummy Data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO vehicle (license_plate, type, owner_name) VALUES
+('ABC-1234', 'CAR', 'Juan Dela Cruz'),
+('XYZ-5678', 'MOTORCYCLE', 'Maria Santos'),
+('DEF-9012', 'TRUCK', 'Pedro Reyes'),
+('GHI-3456', 'CAR', 'Ana Garcia'),
+('JKL-7890', 'MOTORCYCLE', 'Carlos Mendoza'),
+('MNO-2468', 'CAR', 'Sofia Ramos'),
+('PQR-1357', 'TRUCK', 'Miguel Torres'),
+('STU-8642', 'MOTORCYCLE', 'Elena Cruz');
+
+INSERT INTO parking (lot_id, location, capacity, cost_per_minute, occupied_space) VALUES
+('LOT-001', 'Main Entrance', 50, 5, 0),
+('LOT-002', 'North Wing', 30, 4, 0),
+('LOT-003', 'South Wing', 40, 4, 0),
+('LOT-004', 'East Wing', 25, 3, 0),
+('LOT-005', 'West Wing', 35, 3, 0);
